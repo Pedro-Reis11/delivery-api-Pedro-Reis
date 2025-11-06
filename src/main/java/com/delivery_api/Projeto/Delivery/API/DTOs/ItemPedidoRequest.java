6b3dabe397c16e2f312f429a389847349d6b8e62
Pedro-Reis11 <pedroverissimoreis@gmail.com>
2025-11-06 18:27:08 -0300

    Correção estrurural

--- src/main/java/com/delivery_api/Projeto/Delivery/API/DTOs/ItemPedidoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.delivery_api.Projeto.Delivery.API.DTOs;
-import lombok.Data;
-
-@Data
-public class ItemPedidoRequest {
-    private Long produtoId;
-    private Integer quantidade;
-    private String observacoes;  // Opcional: "Sem cebola", etc.
-}
